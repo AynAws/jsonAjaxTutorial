b8adf4e1db103a03dd6db33f0a14eeae6e8ed986
GrimCreeperyt <grimcreeperyt@gmail.com>
2024-10-21 21:56:46 -0400

    tgsedehg

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $('#btn').on('click', () => {
         $.ajax({
-            url: "https://learnwebcode.github.io/json-example/animals-1.json",
+            url: `https://learnwebcode.github.io/json-example/animals-${count}.json`,
             dataType: "json",
             method: "GET",
             success: data => render(data)
@@ -9,15 +9,41 @@ $(document).ready(function() {
     })
     
 })
-let index = -1;
+let index = -1
+let count = 1
 function render(DATA) {
     let skib = '';
 
+    if (index === 2) {
+        index = -1
+        count++
+    }
+
     $('#animal-info').each(() => {
         index++
-        skib += DATA[index].name + " is a " + DATA[index].species + "."
+        skib += DATA[index].name + " is a " + DATA[index].species + " that likes to eat "
+        for (ii = 0; ii < DATA[index].foods.likes.length; ii++) {
+            if (ii === 0) {
+                skib += DATA[index].foods.likes[ii]
+            } else {
+                skib += " and " + DATA[index].foods.likes[ii]
+            }
+        }
+        skib += " and dislikes "
+        for (ii = 0; ii < DATA[index].foods.dislikes.length; ii++) {
+            if (ii === 0) {
+                skib += DATA[index].foods.dislikes[ii]
+            } else {
+                skib += " and " + DATA[index].foods.dislikes[ii]
+            }
+        }
+        skib += '.'
     })
 
     $('#animal-info').append(`<p>${skib}</p>`)
     console.log('render works')
+
+    if (count >= 3 && index === 2) {
+        $('#btn').remove()
+    }
 }
